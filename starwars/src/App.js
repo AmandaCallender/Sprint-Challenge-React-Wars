9332be38efcb737fd6669e579ba6108184feccfd
AmandaCallender <amanda.callender@outlook.com>
2020-03-08 19:56:55 -0400

    Card done

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,31 +1,28 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 import StarwarsDisplay from "./components/starwarsDisplay";
-import styled from 'styled-components';
+import styled from "styled-components";
 
 const ScrollContainer = styled.div`
   height: 100vh;
   overflow: hidden;
-`
+`;
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
 
-  // Fetch characters from the star wars api in an effect hook. Remember, anytime you have a 
+  // Fetch characters from the star wars api in an effect hook. Remember, anytime you have a
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
   return (
-    <div className="App">
-      <h1 className="Header">React Wars</h1>
-    </div>
     <ScrollContainer>
-    <div className="App">
-      <h1 className="Header">React Wars</h1>
+      <div className="App">
+        <h1 className="Header">React Wars</h1>
         <StarwarsDisplay />
-    </div>
-  </ScrollContainer>
+      </div>
+    </ScrollContainer>
   );
-}
+};
 
 export default App;
